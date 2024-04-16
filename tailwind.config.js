07288bc63cb7b1043c3b66d3e04247361e88e2b3
pkthunder87 <pkthunder87@gmail.com>
2024-04-16 08:20:10 -0700

    Style: Add Rubik font to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: 'Rubik',
+    },
+    screens: {
+      sm: '425px',
+      md: '750px',
+      lg: '1024px',
+      xl: '1280px',
+      xl2: '1440px',
+    },
     extend: {},
   },
   plugins: [],
