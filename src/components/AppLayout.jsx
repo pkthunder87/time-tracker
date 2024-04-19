a01fd816ffe082c346c5ff688c17a4d830305695
pkthunder87 <pkthunder87@gmail.com>
2024-04-19 12:21:52 -0700

    Feat: Add initial static grid for desktop design

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,7 +1,27 @@
 function AppLayout() {
   return (
-    <div className=" h-[50dvh] w-[80dvw] bg-neutral-dark-blue text-4xl text-white">
-      Time Tracker
+    <div className=" grid h-[50dvh] w-[80dvw] grid-cols-4 grid-rows-2 gap-5 text-2xl text-white">
+      <div className="row-span-2 flex h-full rounded-xl bg-neutral-dark-blue p-4 text-center">
+        Jeremy Robson
+      </div>
+      <div className=" flex h-full justify-center rounded-xl bg-neutral-dark-blue p-4 text-base">
+        Work
+      </div>
+      <div className=" flex h-full justify-center rounded-xl bg-neutral-dark-blue p-4 text-base">
+        Play
+      </div>
+      <div className=" flex h-full justify-center rounded-xl bg-neutral-dark-blue p-4 text-base">
+        Study
+      </div>
+      <div className=" flex h-full justify-center rounded-xl bg-neutral-dark-blue p-4 text-base">
+        Exercise
+      </div>
+      <div className=" flex h-full justify-center rounded-xl bg-neutral-dark-blue p-4 text-base">
+        Study
+      </div>
+      <div className=" flex h-full justify-center rounded-xl bg-neutral-dark-blue p-4 text-base">
+        Self Care
+      </div>
     </div>
   );
 }
