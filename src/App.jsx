dd978f17413ad5355957f044a1d33771308ca75d
pkthunder87 <pkthunder87@gmail.com>
2024-04-19 12:07:11 -0700

    Feat: Add AppLayout and initial box

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
+import AppLayout from './components/AppLayout';
+
 function App() {
-  return (
-    <div className="text-primary-violet bg-neutral-pale-blue ml-24 mt-24 text-4xl">
-      Time Tracker
-    </div>
-  );
+  return <AppLayout />;
 }
 
 export default App;
---
+++ src/components/AppLayout.jsx
@@ -0,0 +1,9 @@
+function AppLayout() {
+  return (
+    <div className=" h-[50dvh] w-[80dvw] bg-neutral-dark-blue text-4xl text-white">
+      Time Tracker
+    </div>
+  );
+}
+
+export default AppLayout;
