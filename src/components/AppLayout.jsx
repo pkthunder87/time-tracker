e70d73f612a1f5f9708691760ce25ccb839e13cd
pkthunder87 <pkthunder87@gmail.com>
2024-04-21 15:17:39 -0700

    Feat: Finish static for top of UserBox

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -2,24 +2,24 @@ import UserBox from './UserBox';
 
 function AppLayout() {
   return (
-    <div className=" grid h-[50dvh] w-[80dvw] grid-cols-4 grid-rows-2 gap-5 text-2xl text-white">
+    <div className=" grid h-[48.5dvh] w-[77dvw] grid-cols-4 grid-rows-2 gap-[1.2rem] text-white">
       <UserBox />
-      <div className=" flex h-full justify-center rounded-xl bg-neutral-dark-blue p-4 text-base">
+      <div className=" flex h-full justify-center rounded-lg bg-neutral-dark-blue p-4 text-base">
         Work
       </div>
-      <div className=" flex h-full justify-center rounded-xl bg-neutral-dark-blue p-4 text-base">
+      <div className=" flex h-full justify-center rounded-lg bg-neutral-dark-blue p-4 text-base">
         Play
       </div>
-      <div className=" flex h-full justify-center rounded-xl bg-neutral-dark-blue p-4 text-base">
+      <div className=" flex h-full justify-center rounded-lg bg-neutral-dark-blue p-4 text-base">
         Study
       </div>
-      <div className=" flex h-full justify-center rounded-xl bg-neutral-dark-blue p-4 text-base">
+      <div className=" flex h-full justify-center rounded-lg bg-neutral-dark-blue p-4 text-base">
         Exercise
       </div>
-      <div className=" flex h-full justify-center rounded-xl bg-neutral-dark-blue p-4 text-base">
+      <div className=" flex h-full justify-center rounded-lg bg-neutral-dark-blue p-4 text-base">
         Study
       </div>
-      <div className=" flex h-full justify-center rounded-xl bg-neutral-dark-blue p-4 text-base">
+      <div className=" flex h-full justify-center rounded-lg bg-neutral-dark-blue p-4 text-base">
         Self Care
       </div>
     </div>
--- src/components/UserBox.jsx
+++ src/components/UserBox.jsx
@@ -1,7 +1,20 @@
 function UserBox() {
   return (
-    <div className="row-span-2 flex h-full rounded-xl bg-neutral-dark-blue p-4 text-center">
-      Jeremy Robson
+    <div className="row-span-2 grid h-full grid-rows-[68.5%_31.5%] rounded-lg bg-neutral-dark-blue ">
+      <div className="grid grid-rows-[43%_57%] rounded-lg bg-primary-blue p-5">
+        <div className="-ml-[.13rem] mt-[.1rem] h-[3.25rem] w-[3.25rem] rounded-full border-2">
+          <img className="" src="./image-jeremy.png" alt="Profile image" />
+        </div>
+        <div>
+          <p className="text-[.58rem] leading-4 text-neutral-pale-blue">
+            Report for
+          </p>
+          <p className=" text-[1.55rem] font-light leading-[1.8rem]">
+            Jeremy Robson
+          </p>
+        </div>
+      </div>
+      <div className="rounded-b-lg">Bottom</div>
     </div>
   );
 }
