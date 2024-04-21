c9107640d29d631712e39072673aca599ef55c87
pkthunder87 <pkthunder87@gmail.com>
2024-04-20 18:46:51 -0700

    Refactor: Move component code to UserBox.jsx

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,9 +1,9 @@
+import UserBox from './UserBox';
+
 function AppLayout() {
   return (
     <div className=" grid h-[50dvh] w-[80dvw] grid-cols-4 grid-rows-2 gap-5 text-2xl text-white">
-      <div className="row-span-2 flex h-full rounded-xl bg-neutral-dark-blue p-4 text-center">
-        Jeremy Robson
-      </div>
+      <UserBox />
       <div className=" flex h-full justify-center rounded-xl bg-neutral-dark-blue p-4 text-base">
         Work
       </div>
---
+++ src/components/UserBox.jsx
@@ -0,0 +1,9 @@
+function UserBox() {
+  return (
+    <div className="row-span-2 flex h-full rounded-xl bg-neutral-dark-blue p-4 text-center">
+      Jeremy Robson
+    </div>
+  );
+}
+
+export default UserBox;
